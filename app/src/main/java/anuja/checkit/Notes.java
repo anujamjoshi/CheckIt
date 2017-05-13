94d1459607de9dd4ceff2c705ab12975a8c819f8
amjoshi <amjoshi@cpp.edu>
2017-05-12 19:59:49 -0700

    kinda working notes

--- app/src/main/java/anuja/checkit/Notes.java
+++ app/src/main/java/anuja/checkit/Notes.java
@@ -1,41 +1,52 @@
 package anuja.checkit;
-
+import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class Notes extends  ListActivity {
+import anuja.checkit.R;
 
-    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
-    ArrayList<String> listItems=new ArrayList<String>();
-
-    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+public class Notes extends Activity implements OnClickListener
+{
+    private Button btnAdd;
+    private EditText et;
+    private ListView lv;
+    ArrayList<String> list = new ArrayList<String>();
     ArrayAdapter<String> adapter;
 
-    //RECORDING HOW MANY TIMES THE BUTTON HAS BEEN CLICKED
-    int clickCounter=0;
-
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
-        adapter=new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                listItems);
-        setListAdapter(adapter);
+
+        btnAdd = (Button)findViewById(R.id.addTaskBtn);
+        btnAdd.setOnClickListener(this);
+        et = (EditText)findViewById(R.id.editText);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, list);
+
+        // set the lv variable to your list in the xml
+        lv=(ListView)findViewById(R.id.list);
+        lv.setAdapter(adapter);
+    }
+    public void onClick(View v)
+    {
+        String input = et.getText().toString();
+        if(input.length() > 0)
+        {
+            // add string to the adapter, not the listview
+            adapter.add(input);
+            // no need to call adapter.notifyDataSetChanged(); as it is done by the adapter.add() method
+        }
     }
 
-    //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
-    public void addItems(View v) {
-        listItems.add("Clicked : "+clickCounter++);
-        adapter.notifyDataSetChanged();
-    }
 
 }
