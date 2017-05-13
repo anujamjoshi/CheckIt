aea4182645cdce974683553a6f47d1c1c06bd726
amjoshi <amjoshi@cpp.edu>
2017-05-13 01:39:43 -0700

    implement toDo part 1

--- app/src/main/java/anuja/checkit/ToDo.java
+++ app/src/main/java/anuja/checkit/ToDo.java
@@ -1,13 +1,43 @@
 package anuja.checkit;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
-public class ToDo extends AppCompatActivity {
-
+public class ToDo extends Activity  {
+    RelativeLayout ll;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do);
+        ScrollView sv = new ScrollView(this);
+        ll = (RelativeLayout)findViewById(R.id.RelativeLayoutToDO);
+        sv.addView(ll);
+        TextView tv = new TextView(this);
+        EditText et = (EditText)findViewById(R.id.editTodoText);
+//        ll.addView(et);
+        Button b = (Button)findViewById(R.id.addTodoButtonToDo);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for(int i = 0; i < 20; i++) {
+                    CheckBox cb = new CheckBox(getApplicationContext());
+                    cb.setText("I'm dynamic!");
+                    ll.addView(cb);
+                }
+            }
+        });
+
+
     }
+
+
 }
