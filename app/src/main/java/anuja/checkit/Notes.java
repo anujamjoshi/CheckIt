454fcb4c390496012ef9b5e0c7e2cf7df1e1f7ab
amjoshi <amjoshi@cpp.edu>
2017-05-13 12:48:04 -0700

    expandable list view works

--- app/src/main/java/anuja/checkit/Notes.java
+++ app/src/main/java/anuja/checkit/Notes.java
@@ -1,72 +1,78 @@
 package anuja.checkit;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import android.app.Activity;
-import android.app.ListActivity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.ExpandableListView;
 
-import java.util.ArrayList;
-
-import anuja.checkit.R;
-
-public class Notes extends Activity implements AdapterView.OnItemClickListener, View.OnLongClickListener {
-    private Button btnAdd;
-    private EditText et;
-    private ListView lv;
-    ArrayList<String> list ;
-    ArrayAdapter<String> adapter;
+public class Notes extends Activity {
+    private Button btnHeader;
+    private Button btnNote;
+    private EditText etHead;
+    private EditText etNote;
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
 
-        btnAdd = (Button)findViewById(R.id.addTaskBtn);
-        btnAdd.setOnClickListener(new OnClickListener() {
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+    }
+
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+
+        etHead = (EditText)findViewById(R.id.editHeaderText);
+        // Adding child data
+        btnNote = (Button)findViewById(R.id.addnewNoteButton);
+        btnNote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String input = et.getText().toString();
-                if(input.length() > 0)
+                String input = etNote.getText().toString();
+                String header = etHead.getText().toString();
+                if (header.length()<=0){
+                    /*
+                    error checking add the popup saying add a header
+                     */
+                }
+               else if(input.length() > 0  && header.length()>0)
                 {
-                    // add string to the adapter, not the listview
-                    adapter.add(input);
-                    // no need to call adapter.notifyDataSetChanged(); as it is done by the adapter.add() method
+                    if(listDataHeader.indexOf(header)>=0){
+                        // we have an arraylist of this value
+                        listDataChild.get(header).add(input);
+                    }
+                    else{
+                        listDataHeader.add(header);
+                        ArrayList<String> a = new ArrayList<String>();
+                        a.add(input);
+                        listDataChild.put(header, a);
+                    }
                 }
             }
         });
-        et = (EditText)findViewById(R.id.editText);
-        list = new ArrayList<String>();
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, list);
-
-        // set the lv variable to your list in the xml
-        lv=(ListView)findViewById(R.id.listView1);
-
-        lv.setAdapter(adapter);
-        lv.setOnItemClickListener(Notes.this);
-        lv.setOnLongClickListener(Notes.this);
-
+        etNote = (EditText)findViewById(R.id.editNoteContent);
+//
     }
-    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
-        list.remove(position);
-        adapter.notifyDataSetChanged();
-
-    }
-    @Override
-    public boolean onLongClick(View v) {
-
-        Toast.makeText(Notes.this, "To remove...click", Toast.LENGTH_SHORT).show();
-        return false;
-    }
-
-
-
 }
--- app/src/main/java/anuja/checkit/ToDo.java
+++ app/src/main/java/anuja/checkit/ToDo.java
@@ -1,43 +1,69 @@
 package anuja.checkit;
-
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
-import android.widget.ScrollView;
-import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
 
-import java.util.List;
 
-public class ToDo extends Activity  {
-    RelativeLayout ll;
-    EditText et;
-    LinearLayout lv;
+public class ToDo extends Activity implements AdapterView.OnItemClickListener, View.OnLongClickListener {
+    private Button btnAdd;
+    private EditText et;
+    private ListView lv;
+    ArrayList<String> list ;
+    ArrayAdapter<String> adapter;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do);
-        ll = (RelativeLayout)findViewById(R.id.relativeLayoutTodo);
-        lv = (LinearLayout) findViewById(R.id.listViewTodo) ;
-        et = (EditText)findViewById(R.id.editTodoText);
-        Button b = (Button)findViewById(R.id.addTodoBtn);
-        b.setOnClickListener(new View.OnClickListener() {
+
+        btnAdd = (Button)findViewById(R.id.addToDoButton1);
+        btnAdd.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-               CheckBox cb = new CheckBox(getApplicationContext());
-                cb.setText(et.getText());
-                lv.addView(cb);
+                String input = et.getText().toString();
+                if(input.length() > 0)
+                {
+                    // add string to the adapter, not the listview
+                    adapter.add(input);
+                    System.out.println(input);
+                    // no need to call adapter.notifyDataSetChanged(); as it is done by the adapter.add() method
+                }
             }
         });
+        et = (EditText)findViewById(R.id.editTodoTextText);
+        list = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, list);
 
+        // set the lv variable to your list in the xml
+        lv=(ListView)findViewById(R.id.listViewToDo);
+
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(ToDo.this);
+        lv.setOnLongClickListener(ToDo.this);
 
     }
+    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+
+//        list.remove(position);
+        adapter.notifyDataSetChanged();
+
+    }
+    @Override
+    public boolean onLongClick(View v) {
+
+        Toast.makeText(ToDo.this, "To remove...click", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
 
 
 }
